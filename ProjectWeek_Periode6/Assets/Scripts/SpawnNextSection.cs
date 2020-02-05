f5788063245c72f1bbeb81ef82794892d3a84b6a
enzo marguerink <enzomarquerink@gmail.com>
2020-02-05 09:57:10 +0100

    World generator done

---
+++ ProjectWeek_Periode6/Assets/Scripts/SpawnNextSection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNextSection : MonoBehaviour
+{
+    public Transform[] nextSection;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        int randomSection = Random.Range(0, nextSection.Length);
+
+        if (other.tag == "Player")
+        {
+            Debug.Log("Triggered");
+
+            Vector3 spawnPosition = transform.parent.position + new Vector3(0, 0, 18);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(nextSection[randomSection], spawnPosition, spawnRotation);
+        }
+    }
+}
